f440d39f63680bdba9971d58d2edac6bab44068e
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-02-29 12:28:57 +0530

    database server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
+mongoose.connect(process.env.MONGO_DB).then(console.log('Successfully connected to MongoDB')).catch((error)=> console.log(error))
 const app = express()
 
 
