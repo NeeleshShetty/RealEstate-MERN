baf4c1edc1804d4de5baee63c70d2b2e82ff4ff6
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-02-29 13:00:48 +0530

    create Signup Api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_DB).then(console.log('Successfully connected to MongoDB')).catch((error)=> console.log(error))
 const app = express()
 
+app.use(express.json())
 
+app.use('/api/auth',authRouter)
 app.listen(3000, () => {
     console.log("Successfully running on Server 3000");
 })
