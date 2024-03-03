4bbd2364af6e7b81ec6c14301839ba9dd3a212a1
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-03-03 12:21:07 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -2,12 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_DB).then(console.log('Successfully connected to MongoDB')).catch((error)=> console.log(error))
 const app = express()
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/api/auth', authRouter)
 
