ede65567d24c6bc4d65b40362828a063ea913c67
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-03-06 14:04:15 +0530

    edit listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import {createListing,deleteListing} from '../controllers/listing.controllers.js'
+import {createListing,deleteListing,updateListing} from '../controllers/listing.controllers.js'
 import { verifyToken } from '../utils/verifyToken.js'
 const router = express.Router()
 
 router.post('/create', createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
-
+router.post('/update/:id',verifyToken,updateListing)
 
 export default router
