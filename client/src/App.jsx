2162a65916079f20bd3d7624911c7e1d65e8b4f6
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-03-08 19:17:48 +0530

    search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
 					path="/sign-up"
 					element={<Signup />}
 				/>
+				<Route path="/search" element={<Search />} />
 				<Route path="/listing/:listingId" element={<Listing />} />
 				<Route element={<PrivateRoute />}>
 				<Route
