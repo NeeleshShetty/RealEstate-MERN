fe56c812e307ccb5ac1a695c2dc5d7b3b86c6773
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-03-08 15:07:02 +0530

    Add contact landlord functionality to the listing

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 
 import express from 'express'
 import {verifyToken} from '../utils/verifyToken.js'
-import { updateUser,deleteUser,getUserListing} from '../controllers/user.controllers.js'
+import { updateUser,deleteUser,getUserListing,getUser} from '../controllers/user.controllers.js'
 const router = express.Router()
 
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id',verifyToken,getUserListing)
+router.get('/:id',verifyToken,getUser)
 
 export default router
