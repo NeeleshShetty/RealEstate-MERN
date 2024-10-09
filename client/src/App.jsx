6b6916c6ac76742c273213cfc28342a7ea182296
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-10-09 09:11:15 +0530

    added lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,40 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import Signup from "./pages/Signup";
-import PrivateRoute from "./components/PrivateRoute";
-import CreateListing from "./pages/CreateListing";
-import UpdateListing from "./pages/UpdateListing";
-import Listing from "./pages/Listing";
-import Search from "./pages/Search";
+import { lazy, Suspense } from "react";
+
+// Lazy load components
+const About = lazy(() => import("./pages/About"));
+const Home = lazy(() => import("./pages/Home"));
+const Profile = lazy(() => import("./pages/Profile"));
+const SignIn = lazy(() => import("./pages/SignIn"));
+const Signup = lazy(() => import("./pages/Signup"));
+const PrivateRoute = lazy(() => import("./components/PrivateRoute"));
+const CreateListing = lazy(() => import("./pages/CreateListing"));
+const UpdateListing = lazy(() => import("./pages/UpdateListing"));
+const Listing = lazy(() => import("./pages/Listing"));
+const Search = lazy(() => import("./pages/Search"));
 
 function App() {
   return (
-	  <BrowserRouter>
-		  <Header />
-			<Routes>
-				<Route
-					path="/"
-					element={<Home />}
-				/>
-				<Route
-					path="/about"
-					element={<About />}
-				/>
-				
-				<Route
-					path="/sign-in"
-					element={<SignIn />}
-				/>
-				<Route
-					path="/sign-up"
-					element={<Signup />}
-				/>
-				<Route path="/search" element={<Search />} />
-				<Route path="/listing/:listingId" element={<Listing />} />
-				<Route element={<PrivateRoute />}>
-				<Route
-					path="/profile"
-					element={<Profile />}
-				  />
-				  <Route path="/create-Listing" element={<CreateListing />} />
-				  <Route path="/update-Listing/:listingId" element={<UpdateListing />} />
-				</Route>
-				
-			</Routes>
-		</BrowserRouter>
-	);
+    <BrowserRouter>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-listing" element={<CreateListing />} />
+            <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
